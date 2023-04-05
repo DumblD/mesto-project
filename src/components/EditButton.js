59972776a5951d24d8972e0b00261ff97edda347
Artyom Zinin <NicYKN@yandex.ru>
2023-04-05 09:57:24 +0300

    Updates

--- src/components/EditButton.js
+++
@@ -1,9 +0,0 @@
-export default class EditButton {
-  constructor(editButtonSelector, {handleEditButtonClick}) {
-    this._editButton = document.querySelector(editButtonSelector);
-    this._handleEditButtonClick = handleEditButtonClick;
-  }
-  setEventListeners() {
-    this._editButton.addEventListener('click', this._handleEditButtonClick);
-  }
-}
